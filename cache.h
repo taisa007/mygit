f43e7b2bc374d6dcaaa96de4d24e0daa50e94341
Masaki Sato <g5.taisa007@gmail.com>
2015-09-17 00:37:29 +0900

    update

---
+++ cache.h
@@ -0,0 +1,13 @@
+//
+// Created by SatoMasaki on 9/17/15 AD.
+//
+
+#ifndef MYGIT_CACHE_H
+#define MYGIT_CACHE_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
+
+#endif //MYGIT_CACHE_H
--- mygit.c
+++ mygit.c
@@ -1,21 +1,25 @@
 //
 // Created by SatoMasaki on 9/13/15 AD.
 //
-#include <stdio.h>
-#include <sys/stat.h>
-#include <stdlib.h>
-#include <string.h>
+#include "cache.h"
 
 int main(int argc, char *argv[])
 {
-    if (!strcmp("-t", argv[1])) {
-        fprintf(stderr, "引数が不正です\n");
+    if (argc != 2) {
+        fprintf(stderr, "引数を一つ指定してください。");
         exit(1);
     }
 
-    if (mkdir(".mygit", 0755) < 0) {
-        perror("unable to create .mygit directory");
-        exit(1);
+    const char *arg = argv[1];
+
+    if (!strcmp(arg, "init")) {
+        if (mkdir(".mygit", 0755) < 0) {
+            perror("unable to create .mygit directory");
+            exit(1);
+        }
+    } else {
+        printf("view usage");
     }
+
     return 0;
 }
