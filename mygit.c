cd2131506394142991c4cc4fed361a4ca882203c
Masaki Sato <g5.taisa007@gmail.com>
2015-09-16 20:37:43 +0900

    追加

--- mygit.c
+++ mygit.c
@@ -2,8 +2,20 @@
 // Created by SatoMasaki on 9/13/15 AD.
 //
 #include <stdio.h>
+#include <sys/stat.h>
+#include <stdlib.h>
+#include <string.h>
 
 int main(int argc, char *argv[])
 {
-    printf("test");
+    if (!strcmp("-t", argv[1])) {
+        fprintf(stderr, "引数が不正です\n");
+        exit(1);
+    }
+
+    if (mkdir(".mygit", 0755) < 0) {
+        perror("unable to create .mygit directory");
+        exit(1);
+    }
+    return 0;
 }
